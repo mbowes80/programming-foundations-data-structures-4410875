24dff1147cbdcddddf7727d5ad35e3a59d8913d5
mbowes80 <34423867+mbowes80@users.noreply.github.com>
2024-04-13 21:22:56 +0000

    Completed ch. 3 challenge

--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -8,7 +8,14 @@ user_preferences = {
 
 
 def update_preferences(user_pref):
-    return {}
+    # init new dictionary
+    user_preferences_clean = {}
+    # loop over dictionary
+    for key, value in user_preferences.items():
+        # if item is not not, add to new one
+        if value != None:
+            user_preferences_clean[key] = value
+    return user_preferences_clean
 
 
 print(update_preferences(user_preferences))
